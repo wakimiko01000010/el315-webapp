e300484ae98e17fbe3b3ca9dfb98007892cc0706
wakimiko01000010 <link04024946@ezweb.ne.jp>
2022-07-27 04:57:40 +0900

    fix bug

--- app.py
+++ app.py
@@ -1,25 +1,20 @@
 from flask import Flask, render_template
 from flask import request
-from green
+import green
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
+@app.route("/")
 def determiner():
-    engText = request.args["english_text"]
-    if len(engText) == 0:
-        return render_template("determiner.html")
-    else:
-        result = green.article_identifier(engText)
-        return render_template("determiner.html", result=result)
+    return render_template("determiner.html")
 
-'''@app.route("/result", methods=["GET"])
+@app.route("/result", methods=["GET"])
 def receive_get():
     text = request.args["english_text"]
     if len(text) == 0:
         return "no text"
     else:
         return text
-'''
+
 if __name__ == "__main__":
     app.run(debug=True)
