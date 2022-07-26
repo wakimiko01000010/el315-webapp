9596e6b51a7335ed133d492af840a34e15fe07f5
wakimiko01000010 <link04024946@ezweb.ne.jp>
2022-07-26 11:11:54 +0900

    add html

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask import request
 
 app = Flask(__name__)
 
@@ -9,6 +10,17 @@ def hello():
 @app.route("/index")
 def index():
     return render_template("index.html")
+
+@app.route("/app")
+def determiner():
+    return render_template("determiner.html")
+
+def receive_get():
+    text = request.args["english_text"]
+    if len(text) == 0:
+        return "no text"
+    else:
+        return text
 
 if __name__ == "__main__":
     app.run(debug=True)
