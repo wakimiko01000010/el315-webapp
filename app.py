f4b067f5de56e01290c8bbcd52a5f43b8387c77f
wakimiko01000010 <link04024946@ezweb.ne.jp>
2022-07-27 03:10:15 +0900

    change app.py

--- app.py
+++ app.py
@@ -4,14 +4,6 @@ from flask import request
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World"
-
-@app.route("/index")
-def index():
-    return render_template("index.html")
-
-@app.route("/app")
 def determiner():
     return render_template("determiner.html")
 
