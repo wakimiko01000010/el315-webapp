00f4848d9933d0a98fe0fa82ce04c4251b5bd9cc
wakimiko01000010 <Link04024946@ezweb.ne.jp>
2022-08-09 17:48:47 +0900

    add nltk_data

--- green.py
+++ green.py
@@ -3,9 +3,9 @@ import nltk
 from nltk.corpus import cmudict
 from nltk.tag.stanford import StanfordNERTagger
 
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
-nltk.download('cmudict')
+#nltk.download('punkt')
+#nltk.download('averaged_perceptron_tagger')
+#nltk.download('cmudict')
 
 PATH_TO_JAR='./static/stanford-ner-2020-11-17/stanford-ner.jar'
 PATH_TO_MODEL = './static/stanford-ner-2020-11-17/classifiers/english.all.3class.distsim.crf.ser.gz'
